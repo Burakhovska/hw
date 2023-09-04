b8af46b9b48e03316bb0e4352e34308ddd6dd77e
Burakhovska Kateryna <ekaterinastavicenko@gmail.com>
2023-09-04 17:35:35 +0300

    commit-3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -9,14 +9,12 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 const gallery = document.querySelector('.gallery');
 
 const markup = galleryItems.map((img, index) =>
-`<li class="gallery__item" data-img-id="${index}">
-<a class="gallery__link" href="large-image.jpg">
-  <img
-    class="gallery__image"
-    src="${img.preview}"
-    data-source="${img.original}"
-    alt="${img.description}"
-  />
+`<li data-img-id="${index}" class="gallery__item">
+<a class="gallery__link" 
+    href="${img.original}">
+    <img class="gallery__image" 
+    src="${img.preview}" alt="${img.description}"
+    data-description="${img.description}"/>
 </a>
 </li>`);
 
@@ -25,31 +23,34 @@ gallery.insertAdjacentHTML("beforeend", markup.join(''));
 gallery.addEventListener('click', onClick);
 
 function onClick(evt) {
-    evt.preventDefault()
-    if (evt.target.nodeName !== "IMG") {
-        return;
-    } else {
-        const imgId = evt.target.closest('li').dataset.imgId;
-   
-        const currentItem = galleryItems[Number(imgId)];
-        console.log(currentItem);
 
-        const instance = basicLightbox.create(`
-            <div class="modal">
-            <img width="600" src="${currentItem.original}" alt="${currentItem.description}">
-            <h2>"${currentItem.description}</h2>
-            </div>
-        `)
-        instance.show();
-        document.addEventListener("keydown", onKey);
-        function onKey(evt) {
-            if(evt.code === 'Escape') {
-                instance.close()
-                
-            }
-        }
-    }
-}
+    evt.preventDefault()
+
+    
+    let lightbox = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionsData: "data-description",
+        
+    });
+
+
+        let currentItem;
+            if (evt.target.nodeName !== "IMG") {
+                return;
+            } else {
+                const imgId = evt.target.closest('li').dataset.imgId;
+                currentItem = galleryItems[Number(imgId)];
+            };
+
+    lightbox.on('open.simplelightbox', function () {
+        const lightboxContent = lightbox.content();
+        const originalImg = galleryItems.map((img, index) => (`
+        <img width="600" src="${currentItem.original}" alt="${currentItem.description}">
+        <h2 class="description">${currentItem.description}"</h2>`));
+        lightboxContent.innerHTML = originalImg;
+        const description = lightboxContent.querySelector('.description');
+        description.style.opacity = 1;
+    })}
 
 
 console.dir(gallery);
