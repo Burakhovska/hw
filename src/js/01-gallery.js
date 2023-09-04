f4521d7c6cb25b1907cadbae222b1f0e5b305e64
Burakhovska Kateryna <ekaterinastavicenko@gmail.com>
2023-09-04 08:43:33 +0300

    second commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,6 +2,55 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
 
-console.log('hello');
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map((img, index) =>
+`<li class="gallery__item" data-img-id="${index}">
+<a class="gallery__link" href="large-image.jpg">
+  <img
+    class="gallery__image"
+    src="${img.preview}"
+    data-source="${img.original}"
+    alt="${img.description}"
+  />
+</a>
+</li>`);
+
+gallery.insertAdjacentHTML("beforeend", markup.join(''));
+
+gallery.addEventListener('click', onClick);
+
+function onClick(evt) {
+    evt.preventDefault()
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    } else {
+        const imgId = evt.target.closest('li').dataset.imgId;
+   
+        const currentItem = galleryItems[Number(imgId)];
+        console.log(currentItem);
+
+        const instance = basicLightbox.create(`
+            <div class="modal">
+            <img width="600" src="${currentItem.original}" alt="${currentItem.description}">
+            <h2>"${currentItem.description}</h2>
+            </div>
+        `)
+        instance.show();
+        document.addEventListener("keydown", onKey);
+        function onKey(evt) {
+            if(evt.code === 'Escape') {
+                instance.close()
+                
+            }
+        }
+    }
+}
+
+
+console.dir(gallery);
+console.dir(galleryItems);
