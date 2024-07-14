9c698b20f45313d35d8d1811a25a6d1fdfabbe2a
Burakhovska Kateryna <ekaterinastavicenko@gmail.com>
2024-07-14 22:10:05 +0300

    new commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,50 +8,29 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const gallery = document.querySelector('.gallery');
 
-const markup = galleryItems.map((img, index) =>
-`<li data-img-id="${index}" class="gallery__item">
-<a class="gallery__link" 
-    href="${img.original}">
-    <img class="gallery__image" 
-    src="${img.preview}" alt="${img.description}"
-    data-description="${img.description}"/>
-</a>
-</li>`);
+const markup = galleryItems.map(({preview, original, description}) => 
+    `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+);
 
-gallery.insertAdjacentHTML("beforeend", markup.join(''));
+    gallery.insertAdjacentHTML('beforeend', markup.join(''));
 
-gallery.addEventListener('click', onClick);
-
-function onClick(evt) {
-
-    evt.preventDefault()
-
-    
     let lightbox = new SimpleLightbox('.gallery a', {
-        captions: true,
-        captionsData: "data-description",
-        
+        captionPosition: "top",
+        captionsData: 'alt',
+        captionDelay: 250
     });
 
-
-        let currentItem;
-            if (evt.target.nodeName !== "IMG") {
-                return;
-            } else {
-                const imgId = evt.target.closest('li').dataset.imgId;
-                currentItem = galleryItems[Number(imgId)];
-            };
-
-    lightbox.on('open.simplelightbox', function () {
-        const lightboxContent = lightbox.content();
-        const originalImg = galleryItems.map((img, index) => (`
-        <img width="600" src="${currentItem.original}" alt="${currentItem.description}">
-        <h2 class="description">${currentItem.description}"</h2>`));
-        lightboxContent.innerHTML = originalImg;
-        const description = lightboxContent.querySelector('.description');
-        description.style.opacity = 1;
-    })}
-
-
-console.dir(gallery);
-console.dir(galleryItems);
+    lightbox.on('show.simplelightbox', function (evt) {
+            
+    `<div class="gallery">
+        <a href="${original}">
+        <img src="${original}" 
+        alt="${description}" 
+        title="${description}"/></a>
+    </div>`
+    console.dir(evt.target);
+    });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+console.log(player.on);
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+// player.addEventListener('click', onPlay);
+// const onPlay = function(data) {
+//   // data is an object containing properties specific to that event
+//   console.dir(data)
+// };
+
+// player.on('play', onPlay);
+
+{/* <iframe
+  id="vimeo-player"
+  src="https://player.vimeo.com/video/236203659"
+  width="640"
+  height="360"
+  frameborder="0"
+  allowfullscreen
+  allow="autoplay; encrypted-media"
+></iframe> */}
 
