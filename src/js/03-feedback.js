34f9e9207ff2c2be5144416cabe640c65b829b48
Burakhovska Kateryna <ekaterinastavicenko@gmail.com>
2024-07-23 12:23:51 +0300

    finish task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,29 @@
-
-
+import { throttle } from "lodash";
 const feedbackForm = document.querySelector('.feedback-form');
+const localStorageKey = "feedback-form-state";
 
-console.dir(feedbackForm)
+const getKey = localStorage.getItem(localStorageKey);
+if(getKey){
+    const {email, message} = JSON.parse(getKey);
+    feedbackForm.elements.email.value = email|| "";
+    feedbackForm.elements.message.value = message || "";
+}
+
+    feedbackForm.addEventListener("input", throttle(onThrottle, 500));
+    function onThrottle() {
+        const email = feedbackForm.elements.email.value;
+        const message = feedbackForm.elements.message.value;
+        const formValue = {email, message};
+    localStorage.setItem(localStorageKey, JSON.stringify(formValue));
+      };
+      
+      feedbackForm.addEventListener("submit", (evt) => {
+        evt.preventDefault();
+        const email = feedbackForm.elements.email.value;
+        const message = feedbackForm.elements.message.value;
+        const formValue = {email, message};
+        console.log(formValue)
+        localStorage.removeItem(localStorageKey);
+        feedbackForm.reset();
+
+      });
